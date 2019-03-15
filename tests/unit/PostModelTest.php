c384b14d40dcfbd35379d9942959d5f55be2f62c
Bikcode <bikcodeh@gmail.com>
2019-03-14 19:52:26 -0500

    Diferencia entre unitaria e integracion minuto 14

---
+++ tests/unit/PostModelTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Post;
+
+class PostModelTest extends TestCase
+{
+    
+    function test_adding_a_title_generates_a_slug()
+    {
+        $post = new Post([
+            'title' => 'Como instalar Laravel'
+        ]);
+
+        $post->setTittleAttribute($post->title);
+
+        $this->assertSame('como-instalar-laravel', $post->slug);
+    }
+
+    function test_editing_the_file_changes_the_slug()
+    {
+        $post = new Post([
+            'title' => 'Como instalar Laravel'
+        ]);
+
+        $post->title = "Como instalar Laravel 5.1 LTS"; 
+
+        $post->setTittleAttribute($post->title);
+
+        $this->assertSame("como-instalar-laravel-51-lts", $post->slug);
+    }
+}
