0d784a2e4901e30333945a12ff98f70633535caa
Bikcodeh <bikcodeh@gmail.com>
2019-04-01 08:25:19 -0500

    Fin leccion autenticacion de usuario basada en tokens con tdd

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,26 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\{User, Token};
-use Illuminate\Http\Request;
+use App\Token;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function create()
+    public function login(Token $token)
     {
-        return view('login.create');
-    }
+        Auth::login($token->user);
 
-    public function store(Request $request)
-    {
-        $this->validate($request, [
-            'email' => 'required|email|exists:users'
-        ]);
-        $user = User::where('email', $request->get('email'))->first();
+        $token->delete();
 
-        Token::generateFor($user)->sendByEmail();
-
-        alert('Enviamos a tu email un enlace para que inices sesion.');
-        return back();
+        return redirect('/');
     }
 }
