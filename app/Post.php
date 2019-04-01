5bd4478ed60a4469a84955355fb81f08b38a3c23
Bikcodeh <bikcodeh@gmail.com>
2019-04-01 14:50:30 -0500

    Fin migrar de 5.3 a 5.4

---
+++ app/Post.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use GrahamCampbell\Markdown\Facades\Markdown;
+
+class Post extends Model
+{
+    protected $fillable = ['title', 'content'];
+
+    protected $casts = [
+        'pending' => 'boolean'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function subscribers()
+    {
+        return $this->belongsToMany(User::class, 'subscriptions');
+    }
+
+    public function latestComments()
+    {
+        return $this->comments()->orderBy('created_at', 'DESC');
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function getUrlAttribute()
+    {
+        return route('posts.show', [$this->id, $this->slug]);
+    }
+
+    public function getSafeHtmlContentAttribute()
+    {
+        return Markdown::convertToHtml(e($this->content));
+    }
+}
